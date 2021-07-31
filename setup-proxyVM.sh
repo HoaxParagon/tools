8d85ee7358a78402438dcdbe629fcc6116306e2c
HoaxParagon <59360325+HoaxParagon@users.noreply.github.com>
2021-07-31 15:58:46 +0000

    Create setup-proxyVM.sh

---
+++ setup-proxyVM.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ls;
+
+# get the files
+wget https://github.com/tasket/Qubes-vpn-support/archive/refs/heads/master.zip;
+
+# unzip that
+unzip master.zip;
+
+# make vpn dir
+sudo mkdir -p /rw/config/vpn;
+
+# go to fresh dir
+sudo cd /rw/config/vpn;
+
+# get config files
+sudo wget https://www.privateinternetaccess.com/openvpn/openvpn-strong.zip;
+sudo unzip openvpn-strong.zip;
+
+# copy the file to conf
+sudo cp 
