f7d95f6b1e71ef2f34936132226509195e9b9a3b
HoaxParagon <59360325+HoaxParagon@users.noreply.github.com>
2021-07-31 16:04:25 +0000

    Update setup-proxyVM.sh

--- setup-proxyVM.sh
+++ setup-proxyVM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ls;
+cd;
 
 # get the files
 wget https://github.com/tasket/Qubes-vpn-support/archive/refs/heads/master.zip;
@@ -19,4 +19,14 @@ sudo wget https://www.privateinternetaccess.com/openvpn/openvpn-strong.zip;
 sudo unzip openvpn-strong.zip;
 
 # copy the file to conf
-sudo cp 
+sudo cp us_denver.ovpn vpn-client.conf;
+
+# back home
+cd;
+
+# back to run install
+cd Qubes-vpn-support-master;
+
+sudo bash ./install;
+
+
